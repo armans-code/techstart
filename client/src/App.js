0c4ef3ca97fde1be54244a2ec1d66e34020be083
Arman <arman.kumaraswamy@gmail.com>
2023-05-18 21:23:49 -0400

    public

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>TechStart</h1>
+            <p>Work in progress... Tune back in later for more!</p>
+        </div>
+    );
+}
+
+export default App;
